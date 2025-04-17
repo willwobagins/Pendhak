b82b2bbb12ac9743190a8ffc406abed0772f80f5
willwo bagins <tghtriwibowo26@gmail.com>
2025-04-17 15:43:44 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'selamatan-cache-v1';
+const URLsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
